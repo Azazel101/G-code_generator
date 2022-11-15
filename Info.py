7d6ec4d882ca6d04dd8e770efa0fafa75c688d4b
Azazel101 <roman.slovak@gmail.com>
2022-11-15 15:12:15 +0100

    update

--- Info.py
+++ Info.py
@@ -9,7 +9,6 @@ st.set_page_config(
 
 st.write("# Welcome to G-Code Generator! 👋")
 
-
 my_js = """<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="romanslov" data-color="#FFDD00" data-emoji="" data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>"""
 
 st.markdown(
