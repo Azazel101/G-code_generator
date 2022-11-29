18acda59ec669126f544a27f9299beba11ecbc41
Azazel101 <roman.slovak@gmail.com>
2022-11-29 13:52:36 +0100

    update

--- pages/5_➕_Feedrate_Pattern.py
+++ pages/5_➕_Feedrate_Pattern.py
@@ -3,14 +3,14 @@ import os
 from PIL import Image
 
 st.set_page_config(
-    page_title="G-Code Generator : Pattern",
+    page_title="G-Code Generator : Feedrate Pattern",
     layout="wide",
     page_icon="➕",
 )
 
 path = os.path.dirname(__file__)
 
-st.title('G-Code Generator : Pattern')
+st.title('G-Code Generator : Feedrate Pattern')
 
 spindelspeed = st.slider('Spindel Speed', min_value=100,max_value=30000,value=3100, step=100)
 feedrate = st.slider('Feed per tooth', 0.01,1.0,0.1)
